ff01df89b78ee0fc634e5b2471081f7ccd6630c4
Jahan <you@example.com>
2026-08-04 11:01:55 +0330

    [UPDATE] Add New yo making schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
@@ -17,4 +17,6 @@ const userSchema = mongoose.Schema({
     }
 })
 
-export const User = mongoose.model('user', userSchema)
+const User = mongoose.model('user', userSchema)
+
+export default User
